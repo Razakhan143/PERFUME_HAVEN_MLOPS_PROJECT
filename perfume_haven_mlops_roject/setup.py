5a0ae2739b7fbfaa820371ee732ab9eec35d874a
Raza Khan <razakhanzada100@gmail.com>
2025-06-10 18:30:00 +0500

    add new project name mlops perfume haven webiste

---
+++ perfume_haven_mlops_roject/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project perfume haven is for the practice project on mlops i used search recomandation system that uses cosin similarity to figure it out the closeness of search data and display it on the frontend of the website name perfume haven. this project is completey based on the principle of mlops .',
+    author='RAZA KHAN',
+    license='MIT',
+)
